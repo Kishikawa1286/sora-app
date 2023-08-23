98aebe89f563cf5a8cc71ee1bc6b7c21ecd6fff6
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2023-08-23 19:34:12 +0900

    good,bad追加

--- lib/pages/messages/view.dart
+++ lib/pages/messages/view.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sora/pages/card_message/card_message.dart';
 import 'package:sora/pages/messages/view_model.dart';
 
 class MessagesPage extends HookConsumerWidget {
-  const MessagesPage({super.key});
+  const MessagesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final model = ref.watch(messagesPageViewModelProvider);
     final messages = model.messages;
-    final viewModel = ref.read(messagesPageViewModelProvider.notifier);
 
     return Scaffold(
       appBar: AppBar(
@@ -26,28 +26,57 @@ class MessagesPage extends HookConsumerWidget {
 
           final iconUrl = message.senderIconUrl;
 
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CardMessage(message: message),
+          return Slidable(
+            startActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              dismissible: DismissiblePane(onDismissed: () {
+                // Handle dismiss action here
+              }),
+              children: [
+                SlidableAction(
+                  onPressed: (context) {},
+                  backgroundColor: const Color(0xFFFE4A49),
+                  foregroundColor: Colors.white,
+                  icon: Icons.delete,
+                  label: 'Delete',
                 ),
-              );
-            },
-            child: ListTile(
-              leading: iconUrl != null
-                  ? ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Image.network(
-                        iconUrl,
-                        width: 40,
-                        height: 40,
-                      ),
-                    )
-                  : null,
-              title: Text(message.senderName),
-              subtitle: Text(message.createdAt.toDate().toIso8601String()),
+              ],
+            ),
+            endActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: (context) {},
+                  backgroundColor: Color(0xFF0392CF),
+                  foregroundColor: Colors.white,
+                  icon: Icons.save,
+                  label: 'Save',
+                ),
+              ],
+            ),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CardMessage(message: message),
+                  ),
+                );
+              },
+              child: ListTile(
+                leading: iconUrl != null
+                    ? ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          iconUrl,
+                          width: 40,
+                          height: 40,
+                        ),
+                      )
+                    : null,
+                title: Text(message.senderName),
+                subtitle: Text(message.createdAt.toDate().toIso8601String()),
+              ),
             ),
           );
         },
