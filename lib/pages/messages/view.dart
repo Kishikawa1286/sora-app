c664818a6e6fc2cb7a971ec7512e3c37fff4d168
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2023-08-24 16:16:19 +0900

    メッセージ一覧を追加

--- lib/pages/messages/view.dart
+++ lib/pages/messages/view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-// import 'package:sora/pages/card_message/view.dart';
+// import 'package:sora/pages/card_messages/view.dart';
 import 'package:sora/pages/messages/view_model.dart';
 
 class MessagesPage extends HookConsumerWidget {
@@ -11,6 +11,38 @@ class MessagesPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final model = ref.watch(messagesPageViewModelProvider);
     final messages = model.messages;
+
+  String formatWithWeekday(DateTime date) {
+    final now = DateTime.now();
+    final weekdays = ['月', '火', '水', '木', '金', '土', '日'];
+    final weekday = weekdays[
+        date.weekday - 1]; // DateTimeのweekdayは1から7の値を返すため、-1して配列のインデックスとして使用
+
+    // 午前 or 午後の判断
+    final ampm = date.hour < 12 ? '午前' : '午後';
+
+    // 午前・午後表示のための時間調整（13時以降は1から数える）
+    final adjustedHour = date.hour <= 12 ? date.hour : date.hour - 12;
+
+    // 今日の日付と指定された日付が同じであるかどうかを確認
+    if (date.year == now.year &&
+        date.month == now.month &&
+        date.day == now.day) {
+      return '$ampm $adjustedHour:${date.minute.toString().padLeft(2, '0')}';
+    } else {
+      return '${date.month}/${date.day}($weekday)';
+  }
+}
+
+
+    if (messages.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('メッセージ'),
+        ),
+        body: const Center(child: Text('メッセージがありません')), // メッセージがない場合の表示
+      );
+    }
 
     return Scaffold(//全体
       appBar: AppBar(//header
@@ -25,6 +57,7 @@ class MessagesPage extends HookConsumerWidget {
               ),
               child: const Text('All', style: TextStyle(fontSize: 16)),
             ),
+            const SizedBox(width: 10),
             ElevatedButton(
               onPressed: () {},
               style: ButtonStyle(
@@ -34,6 +67,7 @@ class MessagesPage extends HookConsumerWidget {
               ),
               child: const Text('Slack', style: TextStyle(fontSize: 16)),
             ),
+            const SizedBox(width: 10),
             ElevatedButton(
               onPressed: () {},
               style: ButtonStyle(
@@ -89,7 +123,7 @@ class MessagesPage extends HookConsumerWidget {
             ),
             child: ListTile(//中身
               leading: iconUrl != null
-                  ? ClipRRect(
+                  ? ClipRRect(//アイコン
                       borderRadius: BorderRadius.circular(12),
                       child: Image.network(
                         iconUrl,
@@ -98,8 +132,14 @@ class MessagesPage extends HookConsumerWidget {
                       ),
                     )
                   : null,
-              title: Text(message.senderName),
-              subtitle: Text(message.createdAt.toDate().toIso8601String()),
+              title: Row(
+                children: [
+                  Text(message.senderName,style: const TextStyle(fontSize: 16)),
+                  const Expanded(child: SizedBox()),
+                  Text(formatWithWeekday(message.createdAt.toDate()),style: const TextStyle(fontSize: 12)),
+                ],
+              ),
+              subtitle: Text(message.summary,style: const TextStyle(fontSize: 16)),
             ),
           );
         },
