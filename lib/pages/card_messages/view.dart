b8ef07b02ebc1fce8aaaf3661344d4a37ddc2b10
Takuto Kinjo <takuto.takuto.124@gmail.com>
2023-08-24 11:18:50 +0900

    add: 背景に色が付いた

--- lib/pages/card_messages/view.dart
+++ lib/pages/card_messages/view.dart
@@ -42,65 +42,100 @@ class CardMessagesPage extends HookConsumerWidget {
       body: Padding(
         padding: const EdgeInsets.only(top: 32),
         child: CardSwiper(
+          allowedSwipeDirection:
+              AllowedSwipeDirection.only(right: true, left: true),
           cardsCount: messages.length,
           numberOfCardsDisplayed: 3,
           cardBuilder: (context, index, percentThresholdX, percentThresholdY) {
+            print(percentThresholdX);
+            final opacityValue =
+                (percentThresholdX.abs() / 100).clamp(0.0, 1.0);
             final message = messages[index];
             if (message == null) {
               return const SizedBox.shrink();
             }
 
             final iconUrl = message.senderIconUrl;
-            return Card(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 16),
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.8,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ListTile(
-                        leading: iconUrl != null
-                            ? ClipRRect(
-                                borderRadius: BorderRadius.circular(8),
-                                child: Image.network(
-                                  iconUrl,
-                                  width: 40,
-                                  height: 40,
-                                ),
-                              )
-                            : null,
-                        title: Text(message.senderName),
-                        subtitle: Text(
-                          formatWithWeekday(message.createdAt.toDate()),
-                        ),
+            return Stack(
+              children: [
+                // 背後の半円を描画するためのContainer
+                Positioned(
+                  left: percentThresholdX > 0 ? 0 : null,
+                  right: percentThresholdX < 0 ? 0 : null,
+                  top: 0,
+                  bottom: 0,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width *
+                        percentThresholdX.abs(),
+                    decoration: BoxDecoration(
+                      gradient: RadialGradient(
+                        center: percentThresholdX > 0
+                            ? Alignment.centerLeft
+                            : Alignment.centerRight,
+                        colors: [
+                          if (percentThresholdX > 0)
+                            Colors.red.withOpacity(opacityValue)
+                          else
+                            Colors.blue.withOpacity(opacityValue),
+                          Colors.transparent,
+                        ],
+                        stops: const [0, 1],
                       ),
-                      Expanded(
-                        child: SingleChildScrollView(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 16),
-                                child: Text(message.summary),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 16,
-                                  vertical: 8,
-                                ),
-                                child: Text(message.message),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+                Card(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.8,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ListTile(
+                            leading: iconUrl != null
+                                ? ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: Image.network(
+                                      iconUrl,
+                                      width: 40,
+                                      height: 40,
+                                    ),
+                                  )
+                                : null,
+                            title: Text(message.senderName),
+                            subtitle: Text(
+                              formatWithWeekday(message.createdAt.toDate()),
+                            ),
+                          ),
+                          Expanded(
+                            child: SingleChildScrollView(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 16),
+                                    child: Text(message.summary),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 16,
+                                      vertical: 8,
+                                    ),
+                                    child: Text(message.message),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             );
           },
         ),
