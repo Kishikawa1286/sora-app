21a0a2e74d000f4ff17576640536a995ee4647b7
Kentaro Ishikawa <kentaroishikawa1286@gmail.com>
2023-08-25 09:42:04 +0900

    fix code style

--- lib/pages/messages/reply_modal/view.dart
+++ lib/pages/messages/reply_modal/view.dart
@@ -35,75 +35,77 @@ class ReplyModalContent extends HookConsumerWidget {
     final text = viewModel.text;
 
     return Padding(
-  padding: const EdgeInsets.only(left: 30, right: 30),
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      const Padding(
-        padding: EdgeInsets.only(bottom: 13),
-        child: Text(
-          'このメッセージで返信しますか?',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+      padding: const EdgeInsets.only(left: 30, right: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(bottom: 13),
+            child: Text(
+              'このメッセージで返信しますか?',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ),
-      ),
-      Container(
-        width: 370,
-        decoration: BoxDecoration(
-          color: const Color(0xFFFBFCFF),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 332,
-                padding: const EdgeInsets.all(20),
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Text(text, style: const TextStyle(fontSize: 16)),
+          Container(
+            width: 370,
+            decoration: BoxDecoration(
+              color: const Color(0xFFFBFCFF),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 332,
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(text, style: const TextStyle(fontSize: 16)),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+          // Moved the Row outside the Container
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      onCanceled?.call();
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('キャンセル'),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      onReplied?.call();
+                      await viewModel.reply();
+                    },
+                    child: const Text('決定'),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
-      // Moved the Row outside the Container
-      Padding(
-        padding: const EdgeInsets.only(top:15,),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: ElevatedButton(
-                onPressed: () {
-                  onCanceled?.call();
-                  Navigator.of(context).pop();
-                },
-                child: const Text('キャンセル'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: ElevatedButton(
-                onPressed: () async {
-                  onReplied?.call();
-                  await viewModel.reply();
-                },
-                child: const Text('決定'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ],
-  ),
-);
-}
+    );
+  }
 }
